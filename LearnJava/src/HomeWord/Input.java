82464d1be8378a3f75200bf399c611f398be8c78
huypham <huy5153a@gmail.com>
2022-11-14 14:31:37 +0700

    update

---
+++ LearnJava/src/HomeWord/Input.java
@@ -0,0 +1,30 @@
+package HomeWord;
+import java.awt.*;
+import java.util.Scanner;
+public class Input {
+    public static void main(String[] args){
+        System.out.println("Input java");
+        Scanner Input = new Scanner(System.in);
+        String name,mssv;
+        int age;
+        int yearOfbrith;
+        int mediumScore;
+        System.out.print("Nhập vào họ và têm : ");
+        name = Input.nextLine();
+        System.out.print("Nhập vào mã số sinh viên : ");
+        mssv = Input.nextLine();
+        System.out.print("Nhập vào tuổi : ");
+        age = Input.nextInt();
+        System.out.print("Nhập vào năm sinh : ");
+        yearOfbrith = Input.nextInt();
+        System.out.print("Nhập vào điểm trung bình : ");
+        mediumScore = Input.nextInt();
+        System.out.println("----------=====Thông tin sin viên=====----------");
+        System.out.println("Họ và tên : "+name);
+        System.out.println("Mã số sinh viên : "+mssv);
+        System.out.println("Tuổi : "+age);
+        System.out.println("Năm sinh : "+yearOfbrith);
+        System.out.println("Điểm trung bình : "+mediumScore);
+        System.out.println("Cảm ơn bạn đã cung cấp thông tin : "+mediumScore);
+    }
+}
---
+++ LearnJava/src/HomeWord/get_acquainted_with_java.java
@@ -0,0 +1,11 @@
+package HomeWord;
+
+public class get_acquainted_with_java {
+    public static void main(String[] args){
+        String name = "Phạm tuấn huy";
+        int age = 19;
+        System.out.println("Hello! I'm "+name);
+        System.out.println("I'm "+age+"year old");
+        System.out.println("This is my first java program");
+    }
+}
